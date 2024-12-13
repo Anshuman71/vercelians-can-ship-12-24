8782902cbb790e5fe17dddc4c05563ffebd5e15b
Anshuman Bhardwaj <anshu5074@gmail.com>
2024-12-13 15:42:27 +0530

    add new files

--- next.config.js
+++ next.config.js
@@ -1,14 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        // domains: ['avatars.githubusercontent.com'],
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**.githubusercontent.com',
-              },
-        ]
-    }
-}
+  images: {
+    // domains: ['avatars.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.githubusercontent.com",
+      },
+    ],
+  },
+  rewrites: () => {
+    const rewrites = [];
 
-module.exports = nextConfig
+    rewrites.push({
+      source: "/siteb{/}?",
+      destination: "https://rrv7.vercel.app",
+    });
+    rewrites.push({
+      source: "/siteb/:path*",
+      destination: "https://rrv7.vercel.app/office",
+    });
+    rewrites.push({
+      source: "/testing{/}?",
+      destination: `https://www.google.com`,
+    });
+
+    return {
+      beforeFiles: rewrites,
+    };
+  },
+};
+
+module.exports = nextConfig;
